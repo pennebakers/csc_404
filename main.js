8357079e400a8f98a399b9c60346cb33004cae15
Tom B <thomasbarrett603@hotmail.com>
2020-02-27 20:45:05 -0500

    Initial Commit +1

--- main.js
+++ main.js
@@ -1,33 +1,23 @@
 const prompt = require('prompt');
 
-const properties = [
-    {
-        name: 'CSC141'
-    },
-    {
-        name: 'CSC142'
-    },
-    {
-        name: 'CSC241'
-    },
-    {
-        name: 'CSC242'
-    }
-];
-
 prompt.start();
 
 console.log('Enter your numerical grades for the following classes');
 
-prompt.get(properties, function (err, result) {
+prompt.get(['name', 'CSC141', 'CSC142', 'CSC241', 'CSC242'], function (err, result) {
     if (err) {
         return onErr(err);
     }
     console.log('Command-line input received:');
-    console.log('  CSC141: ' + result.grade);
-    console.log('  CSC142: ' + result.csc142);
-    console.log('  CSC241: ' + result.csc241);
-    console.log('  CSC242: ' + result.csc242);
+    console.log('  Name: ' + result.name);
+    console.log('  CSC141: ' + result.CSC141);
+    console.log('  CSC142: ' + result.CSC142);
+    console.log('  CSC241: ' + result.CSC241);
+    console.log('  CSC242: ' + result.CSC242);
+
+    let x = result.CSC141 + result.CSC142 + result.CSC241 + result.CSC242;
+    let gpa = x / 4;
+    console.log(' GPA: ' + gpa);
 });
 
 function onErr(err) {
