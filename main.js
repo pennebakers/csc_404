b433a3dbeb8c17ccc216b5a6645c8d58c607c9d9
tb603 <tb886379@wcupa.edu>
2020-03-03 00:02:46 -0500

    0.0.1

--- main.js
+++ main.js
@@ -1,24 +1,68 @@
 const prompt = require('prompt');
 
-prompt.start();
-
-console.log('Enter your numerical grades for the following classes');
-
-prompt.get(['name', 'CSC141', 'CSC142', 'CSC241', 'CSC242'], function (err, result) {
-    if (err) {
-        return onErr(err);
+const classes = {
+    properties: {
+        Name: {
+            name: 'name',
+        },
+        CSC141: {
+            name: 'csc141',
+        },
+        CSC142: {
+            name: 'csc142'
+        },
+        CSC241: {
+            name: 'csc241'
+        },
+        CSC242: {
+            name: 'csc242'
+        }
     }
-    console.log('Command-line input received:');
-    console.log('  Name: ' + result.name);
-    console.log('  CSC141: ' + result.CSC141);
-    console.log('  CSC142: ' + result.CSC142);
-    console.log('  CSC241: ' + result.CSC241);
-    console.log('  CSC242: ' + result.CSC242);
+};
 
-    let x = parseInt(result.CSC141) + parseInt(result.CSC142) + parseInt(result.CSC241) + parseInt(result.CSC242);
-    let gpa = parseFloat(x / 4).toFixed(1);
-    console.log(' GPA: ' + gpa);
-});
+getStudent();
+
+function getStudent() {
+    var grades = {
+        'A': 4.0,
+        'A-': 3.67,
+        'B+': 3.33,
+        'B': 3.0,
+        'B-': 2.67,
+        'C+': 2.33,
+        'C': 2.0,
+        'C-': 1.67,
+        'D+': 1.33,
+        'D': 1.00,
+        'D-': 0.67,
+        'F': 0.0
+    };
+
+    prompt.start();
+    console.log('Enter your letter grades for the following classes');
+
+
+    prompt.get(classes, function (err, result) {
+        if (err) {
+            return onErr(err);
+        }
+        let gpa = 0.0;
+
+        gpa = gpa + grades[result.CSC141];
+        gpa = gpa + grades[result.CSC142];
+        gpa = gpa + grades[result.CSC241];
+        gpa = parseFloat(gpa + grades[result.CSC242]);
+        gpa = gpa / 4;
+
+        console.log('Command-line input received:');
+        console.log('  Name: ' + result.Name);
+        console.log('  CSC141: ' + result.CSC141);
+        console.log('  CSC142: ' + result.CSC142);
+        console.log('  CSC241: ' + result.CSC241);
+        console.log('  CSC242: ' + result.CSC242);
+        console.log('  GPA: ' + gpa);
+    });
+}
 
 function onErr(err) {
     console.log(err);
